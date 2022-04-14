4ada5f98368b4e1c3f5fa45c4d6b484331f39431
Julian <perassijulian@gmail.com>
2022-04-14 12:57:41 -0300

    add contact form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,7 +2,17 @@ import './contact.scss';
 
 const Contact = () => {
   return (
-    <div className='contact' id='contact'>Contact</div>
+    <div className='contact' id='contact'>
+      <div className='left'></div>
+      <div className='right'>
+        <h1>Contact</h1>
+        <form>
+          <input type='email' placeholder='email'></input>
+          <textarea placeholder='comments'></textarea>
+          <button type='submit'>SUBMIT</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
