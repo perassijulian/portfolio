9df4bedd510ca3ef67dd3d241e6a2a90dbbb94b9
Julian Perassi <83669055+perassijulian@users.noreply.github.com>
2025-04-26 14:30:49 -0300

    UI refactor

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,37 +1,57 @@
 import { useState } from "react";
-import emailjs from 'emailjs-com';
+import emailjs from "emailjs-com";
 import "./contact.scss";
 
 export default function Contact() {
   const [email, setEmail] = useState("");
-  const [message, setMessage] = useState(false);
+  const [message, setMessage] = useState("");
   const [messageSent, setMessageSent] = useState(false);
+
+  const [isSending, setIsSending] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     if (!message) {
-      alert('Please write something 🙏')
-    } else if (!email) {
-      alert('Please add your email 🙏')
-    } else {
-      const templateParams = {
-        email,
-        message,
-      };
-      
-      emailjs.send("service_dir750i", "template_m224s1c", templateParams, "k2O5a1s8kkwPa8qoE")
-        .then((response) => {
-          console.log('SUCCESS!', response.status, response.text);
+      alert("Please write something 🙏");
+      return;
+    }
+
+    if (!email) {
+      alert("Please add your email 🙏");
+      return;
+    }
+
+    setIsSending(true);
+
+    const templateParams = {
+      email,
+      message,
+    };
+
+    emailjs
+      .send(
+        "service_dir750i",
+        "template_m224s1c",
+        templateParams,
+        "k2O5a1s8kkwPa8qoE"
+      )
+      .then(
+        (response) => {
+          console.log("SUCCESS!", response.status, response.text);
           setEmail("");
           setMessage("");
           setMessageSent(true);
-        }, (err) => {
-          console.log('FAILED...', err);
-        });
-    }
+        },
+        (err) => {
+          console.log("FAILED...", err);
+        }
+      )
+      .finally(() => {
+        setIsSending(false);
+      });
   };
-  
+
   return (
     <div className="contact" id="contact">
       <div className="left">
@@ -39,13 +59,23 @@ export default function Contact() {
       </div>
       <div className="right">
         <h2>Contact.</h2>
-        {!messageSent && 
-        <form onSubmit={handleSubmit}>
-          <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
-          <textarea placeholder="Message" onChange={(e) => setMessage(e.target.value)}></textarea>
-          <button type="submit">Send</button>
-        </form>
-        }
+        {!messageSent && (
+          <form onSubmit={handleSubmit}>
+            <input
+              type="email"
+              placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <textarea
+              placeholder="Message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            ></textarea>
+            <button type="submit" disabled={isSending}>
+              {isSending ? "Sending.." : "Send"}
+            </button>
+          </form>
+        )}
         {messageSent && <span>Thanks, I'll reply ASAP :)</span>}
       </div>
     </div>
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,11 +1,7 @@
 import { useEffect, useState } from "react";
 import PortfolioList from "../portfolioList/PortfolioList";
 import "./portfolio.scss";
-import {
-  backendData,
-  web3Data,
-  frontendData,
-} from '../../data';
+import { backendData, web3Data, frontendData } from "../../data";
 
 export default function Portfolio() {
   const [selected, setSelected] = useState("web3");
@@ -13,31 +9,31 @@ export default function Portfolio() {
   const list = [
     {
       id: "web3",
-      title: "Web3"
+      title: "Web3",
     },
     {
       id: "frontend",
-      title: "Front End"
+      title: "Front End",
     },
     {
       id: "backend",
-      title: "Back End"
-    },   
+      title: "Back End",
+    },
   ];
 
   useEffect(() => {
     switch (selected) {
-      case 'web3':
-        setData(web3Data)
+      case "web3":
+        setData(web3Data);
         break;
-      case 'frontend':
-        setData(frontendData)
+      case "frontend":
+        setData(frontendData);
         break;
-      case 'backend':
-        setData(backendData)
+      case "backend":
+        setData(backendData);
         break;
       default:
-        setData(web3Data)
+        setData(web3Data);
         break;
     }
   }, [selected]);
@@ -58,11 +54,8 @@ export default function Portfolio() {
       </ul>
       <div className="container">
         {data.map((d, i) => (
-          <div className="item" key={i}>
-            <img
-              src={d.img}
-              alt=""
-            />
+          <div className="item" key={d.title}>
+            <img src={d.img} alt="" />
             <h3>{d.title}</h3>
           </div>
         ))}
