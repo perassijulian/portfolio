ca2c47cc15c90e292eac2a97a2afb6e1d759df6d
Julian Perassi <83669055+perassijulian@users.noreply.github.com>
2025-06-30 18:34:12 -0300

    fixed projects overflowing when on mobile

--- components/UI/Project.tsx
+++ components/UI/Project.tsx
@@ -27,16 +27,16 @@ export default function Project({
   return (
     <Card>
       <div className="flex items-center mt-4 gap-4 md:flex-row flex-col-reverse">
-        <div className="md:w-1/3">
+        <div className="w-full md:w-1/3">
           <Image
-            className="rounded-lg border"
+            className="rounded-lg border w-full h-auto"
             src={img}
             alt={alt}
             width={800}
             height={600}
           />
         </div>
-        <div className="md:w-2/3">
+        <div className="w-full md:w-2/3">
           <h1 className="text-foreground-secondary font-bold group-hover:text-accent transition-colors duration-300">
             {title}
             <ArrowUpRight
@@ -45,7 +45,7 @@ export default function Project({
             />
           </h1>
           <p className="mt-4 text-justify">{desc}</p>
-          <div className="flex gap-2 mt-4">
+          <div className="flex flex-wrap gap-2 mt-4">
             {tags.map((tag, i) => (
               <Pill tagKey={`${id}-${i}`} key={`${id}-${i}`}>
                 {tag}
