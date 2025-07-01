a4af7a5c3017491c3848f377c3de7b103387b502
Julian Perassi <83669055+perassijulian@users.noreply.github.com>
2025-07-01 20:28:25 -0300

    add border when hover Pill

--- components/UI/Pill.tsx
+++ components/UI/Pill.tsx
@@ -6,10 +6,11 @@ interface PillProps {
 export default function Pill({ children, tagKey }: PillProps) {
   return (
     <div
-      className="text-accent bg-accent-muted px-4 py-1 rounded-2xl"
+      className="relative px-4 py-1 rounded-2xl text-accent bg-accent-muted cursor-default transition-all group"
       key={tagKey}
     >
       {children}
+      <div className="absolute inset-0 rounded-2xl border border-transparent group-hover:border-accent" />
     </div>
   );
 }
