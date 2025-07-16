bf395fc52e340e60b68da6f488c0bd2c2e52debd
Julian Perassi <83669055+perassijulian@users.noreply.github.com>
2025-07-16 10:17:51 -0300

    add observer to track scrolling

--- components/NavigationLinks.tsx
+++ components/NavigationLinks.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import UnderlineButton from "./UI/UnderlineButton";
 
 const sections = [
@@ -12,6 +12,34 @@ const sections = [
 
 export default function NavigationLinks() {
   const [active, setActive] = useState("about");
+
+  useEffect(() => {
+    const handleScrollSpy = (entries: IntersectionObserverEntry[]) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setActive(entry.target.id);
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(handleScrollSpy, {
+      root: null,
+      rootMargin: "0px 0px -60% 0px",
+      threshold: 0.1,
+    });
+
+    sections.forEach((section) => {
+      const element = document.getElementById(section.id);
+      if (element) observer.observe(element);
+    });
+
+    return () => {
+      sections.forEach((section) => {
+        const element = document.getElementById(section.id);
+        if (element) observer.unobserve(element);
+      });
+    };
+  }, []);
 
   const handleClick = (id: string) => {
     setActive(id);
